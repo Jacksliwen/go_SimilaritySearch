9c035e8e1d9a99d9ed198da9f524577665011cd2
yang.yang <yang.yang@shgpu-t4-005.xforwardai.io>
2020-12-09 11:48:26 +0800

    add faiss 1.6.3

--- searchengine/include/faissengine.h
+++ searchengine/include/faissengine.h
@@ -1,5 +1,48 @@
 #ifndef ___FAISSENGINE___
 #define ___FAISSENGINE___
-void hello(void);
+#include <iostream>
+#include <mutex>
+#include <utility>
+#include "faiss/IndexFlat.h"
+#include "faiss/gpu/GpuAutoTune.h"
+#include "faiss/gpu/GpuCloner.h"
+#include "faiss/gpu/GpuClonerOptions.h"
+#include "faiss/gpu/GpuIndexFlat.h"
+#include "faiss/gpu/StandardGpuResources.h"
+#include "faiss/gpu/GpuCloner.h"
+#include "faiss/gpu/GpuClonerOptions.h"
+#include "faiss/gpu/utils/DeviceUtils.h"
+#include <malloc.h>
+class FaissEngine {
+ public:
+ FaissEngine(const int feature_size) { feature_size_ = feature_size; }
+
+  ~FaissEngine() { Close(); }
+
+  //初始化
+  bool Init();
+
+  
+  void Close();
+
+  //添加带检索数据
+  bool Add(const float *feats, int nums);
+
+  //检索
+  bool Search(const float *feat,int num, int top_N,
+                         std::vector<std::vector<std::pair<int64_t, float>>> *result);
+
+ private:
+  int feature_size_;
+  int feature_nums_;
+  int gpu_nums_;
+  std::mutex mutex_;
+  std::vector<int> devs_;
+  faiss::Index *gpu_index_;
+  faiss::IndexFlatL2 *cpu_index_;
+  faiss::gpu::GpuMultipleClonerOptions co_;
+  std::vector<faiss::gpu::GpuResources *> gpu_resource_;
+};
+
 #endif
 
--- searchengine/searchengine.go
+++ searchengine/searchengine.go
@@ -4,9 +4,30 @@ package searchengine
 #cgo  CFLAGS:  -I${SRCDIR}/include
 #cgo  LDFLAGS:  -L${SRCDIR}/lib -lfaissengine
 #include "faissengine.h"
+#ifdef __cplusplus
+extern "C" {
+	#endif
+	bool Init(){
+		FaissEngine *engine = new FaissEngine(256);
+		if(engine->Init()){
+			printf("c++ init OK \n");
+			return true;
+		}
+		printf("c++ init Failed \n");
+		return false;
+	}
+
+	#ifdef __cplusplus
+}
+#endif
 */
 import "C"
+import (
+	"fmt"
+)
 
-func SayHello() {
-	C.hello()
+func Init() {
+	if C.Init() == true {
+		fmt.Println("go   init ok")
+	}
 }
